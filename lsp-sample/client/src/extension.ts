636b07412ecc986506625feb9d77e33e8a3b4b15
Aaron Prindle <aprindle@google.com>
2021-11-09 13:55:10 -0800

    skaffold lsp client changes

--- lsp-sample/client/src/extension.ts
+++ lsp-sample/client/src/extension.ts
@@ -5,44 +5,60 @@
 
 import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
+import * as net from "net";
 
 import {
 	LanguageClient,
 	LanguageClientOptions,
 	ServerOptions,
+	StreamInfo,
 	TransportKind
 } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
+	let connectionInfo = {
+		port: 4389,
+		host: "127.0.0.1"
+    };
+
 	// The server is implemented in node
-	const serverModule = context.asAbsolutePath(
+	let serverModule = context.asAbsolutePath(
 		path.join('server', 'out', 'server.js')
 	);
 	// The debug options for the server
 	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
-	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
+	// let serverOptions: ServerOptions = {
+	// 	run: { module: serverModule, transport: TransportKind.socket },
+	// 	debug: {
+	// 		module: serverModule,
+	// 		transport: TransportKind.socket,
+	// 		options: debugOptions
+	// 	}
+	// };
+
+	let serverOptions = () => {
+        // Connect to language server via socket
+        let socket = net.connect(connectionInfo);
+        let result: StreamInfo = {
+            writer: socket,
+            reader: socket
+        };
+        return Promise.resolve(result);
+    };
 
 	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
+	let clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
 		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
 		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
+            fileEvents: workspace.createFileSystemWatcher('**/*.*')
+        }
 	};
 
 	// Create the language client and start the client.
